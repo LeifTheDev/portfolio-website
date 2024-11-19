9ee9374c62e702ce5f5d86f5f7d04195d754ecc5
LEIF BAYLEY <BAYLEL@tdm.local>
2024-11-19 15:16:04 +0800

    feat: Add contact form

--- assets/js/contact_scripts.js
+++ assets/js/contact_scripts.js
@@ -5,3 +5,31 @@ function isValidPhone(number) {
         ) !== null
     );
 }
+
+function isValidEmail(email) {
+    return email.match(
+        `[a-z0-9!#$%&'*+/=?^_\`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?` !==
+            null
+    );
+}
+
+function isValidMessage(message, minLength, maxLength) {
+    if (message.replace(" ", "") === "") {
+        return false;
+    }
+
+    if (message.length > maxLength || message.Length < minLength) {
+        return false;
+    }
+
+    return true;
+}
+
+function sendMessage(email, phone, subject, message) {}
+
+function submitForm() {}
+
+document.addEventListener("submit", (event) => {
+    submitForm();
+    event.preventDefault();
+});
